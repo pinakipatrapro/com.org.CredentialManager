bffc976521bd9095a2fa3e9215a69daa8510160d
Pinaki Patra <pinaki.patra.pro@gmail.com>
2018-06-06 11:53:15 +0530

    Add test f or search

--- src/test/java/com/org/UI5ExploredBasicStep/CreateTest.java
+++ src/test/java/com/org/UI5ExploredBasicStep/CreateTest.java
@@ -14,45 +14,51 @@ public class CreateTest {
 	static LoadDrivers drivers = new LoadDrivers();
 	private static final WebDriver wd = drivers.openUrl("http://127.0.0.1:5500/LocalStorage/WebContent/index.html");;
 	private static final ElementHelper eh = new ElementHelper();
-	String randGenStr;
-	
+	public String randGenStr;
+
 	//Generate random string
 	public String randomString(int length) {
 		String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-		  Random rand = new Random();
-		  StringBuilder buf = new StringBuilder();
-		  for (int i=0; i<length; i++) {
-		    buf.append(chars.charAt(rand.nextInt(chars.length())));
-		  }
-		  return buf.toString();
+		Random rand = new Random();
+		StringBuilder buf = new StringBuilder();
+		for (int i=0; i<length; i++) {
+			buf.append(chars.charAt(rand.nextInt(chars.length())));
 		}
-	
+		return buf.toString();
+	}
+
 	@Test(priority = 1)
 	public void navToCreateCredPage() {
 		String buttonPath = "//button[contains(.,'Create Entry')]";
 		eh.getElementByXpath(buttonPath,wd).click();
 	}
-	
+	public void clearData() {
+		eh.getElementByXpath("//input[@placeholder='Domain']",wd).clear();
+		eh.getElementByXpath("//input[@placeholder='Username']",wd).clear();
+		eh.getElementByXpath("//input[@placeholder='Password']",wd).clear();
+		eh.getElementByXpath("//textarea[@placeholder='Description']",wd).clear();
+	}
 	@Test(priority = 2)
 	public void fillData() {
 		System.out.println("Filling random data");
+		clearData();
 		randGenStr = randomString(10);
 		eh.getElementByXpath("//input[@placeholder='Domain']",wd).sendKeys(randGenStr);
 		eh.getElementByXpath("//input[@placeholder='Username']",wd).sendKeys(randGenStr);
 		eh.getElementByXpath("//input[@placeholder='Password']",wd).sendKeys(randGenStr);
 		eh.getElementByXpath("//textarea[@placeholder='Description']",wd).sendKeys(randomString(20));
 	}
-	
+
 	@Test(priority = 3)
 	public void resetFilledData() {
 		System.out.println("Resetting filled data and re-filling");
 		eh.getElementByXpath("//button[contains(.,'Reset')]",wd).click();
-	}
-	
-	@Test(priority = 4)
-	public void saveData() {
 		//Press cancel
 		eh.getElementByXpath("//button[contains(.,'Cancel')]",wd).click();
+	}
+
+	@Test(priority = 4)
+	public void saveData() {
 		//Go back to home and navigate again to create
 		navToCreateCredPage();
 		fillData();
@@ -63,8 +69,41 @@ public class CreateTest {
 		WebElement savedItem = eh.getElementByXpath("//*[contains(.,'"+randGenStr+"')]",wd);
 		Assert.assertEquals(true, savedItem.isDisplayed());
 	}
-	
-	@Test(priority = 99999)
+
+	@Test(priority = 5)
+	public void editCreatedData() {
+		eh.getElementByXpath("//li[contains(.,'"+randGenStr+"')]",wd).click();
+		eh.getElementById("__button7-inner",wd).click();
+		fillData();
+		eh.getElementById("__button4-inner",wd).click();
+		System.out.println("Check if the saved data "+randGenStr+" is avilable after refreshing the page");
+		wd.navigate().refresh();
+		WebElement savedItem = eh.getElementByXpath("//*[contains(.,'"+randGenStr+"')]",wd);
+		Assert.assertEquals(true, savedItem.isDisplayed());
+	}
+	@Test(priority = 6)
+	public void createRandomRecordsAndSearch() {
+		wd.navigate().refresh();
+		for(int i = 0;i < 3; i++) {
+			saveData();	
+		}
+		randGenStr = "ActualSearchStringForTesting";
+		navToCreateCredPage();
+		eh.getElementByXpath("//input[@placeholder='Domain']",wd).sendKeys(randGenStr);
+		eh.getElementByXpath("//input[@placeholder='Username']",wd).sendKeys(randGenStr);
+		eh.getElementByXpath("//input[@placeholder='Password']",wd).sendKeys(randGenStr);
+		eh.getElementByXpath("//textarea[@placeholder='Description']",wd).sendKeys(randGenStr);
+		
+		eh.getElementByXpath("//button[contains(.,'Save')]",wd).click();
+		wd.navigate().refresh();
+		//Search for the explicitly created item
+		eh.getElementByXpath("//input[@placeholder='Search']",wd).sendKeys(randGenStr.substring(5));
+		WebElement savedItem = eh.getElementByXpath("//*[contains(.,'"+randGenStr+"')]",wd);
+		Assert.assertEquals(true, savedItem.isDisplayed());
+
+	}
+
+	@Test(priority = 9999)
 	public void closeTab() {
 		System.out.println("Waiting for the tab to close");
 		try {
@@ -72,6 +111,6 @@ public class CreateTest {
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		wd.close();
+//		wd.close();
 	}
 }
